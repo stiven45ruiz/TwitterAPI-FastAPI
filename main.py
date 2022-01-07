2ab9c74e098701725cd5359c460490f3f791c243
stiven45ruiz <stiven45ruiz@gmail.com>
2022-01-07 11:50:05 -0500

    model_tweets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Python
 from uuid import UUID
-from datetime import date
+from datetime import date 
+from datetime import datetime
 from typing import Optional
 
 #Pydontic
@@ -20,7 +21,8 @@ class UserBase(BaseModel):
 class UserLogin(UserBase):
     password: str = Field(
         ...,
-        min_length=8
+        min_length=8,
+        max_length=64
     )
 
 class User(UserBase):
@@ -40,7 +42,15 @@ class User(UserBase):
 
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...),
+    content: str = Field(
+        ..., 
+        min_length=1, 
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 
 @app.get(path="/")

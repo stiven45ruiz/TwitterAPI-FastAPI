5f1ac109c1d3ada52a6309eb755263a4f1b9ada8
stiven45ruiz <stiven45ruiz@gmail.com>
2022-01-07 12:21:17 -0500

    path_operation_structure_users

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from uuid import UUID
 from datetime import date 
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 #Pydontic
 from pydantic import BaseModel
@@ -10,6 +10,7 @@ from pydantic import EmailStr
 from pydantic import Field
 #FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
@@ -40,7 +41,6 @@ class User(UserBase):
     )
     birt_date: Optional[date] = Field(default=None)
 
-
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...),
     content: str = Field(
@@ -52,7 +52,76 @@ class Tweet(BaseModel):
     update_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
+#Path Operations
 
 @app.get(path="/")
 def home():
     return{"Twitter API": "Working"}
+
+
+##Users
+
+@app.post(
+    path="/singup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+)
+def singup():
+    pass
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+)
+def login():
+    pass
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all User",
+    tags=["Users"]
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=["Users"]
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/user/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=["Users"]
+)
+def delete_a_user():
+    pass
+
+@app.put(
+    path="/user/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=["Users"]
+)
+def delete_a_user():
+    pass
+
+## Tweets
+
+
+
